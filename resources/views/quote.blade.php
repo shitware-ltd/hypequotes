c6d16da150098f3ecadf99699e54c7e19e37f389
Mathias Jørgensen <mathias.jorqensen@gmail.com>
2024-10-26 03:39:25 +0200

    new design

--- resources/views/quote.blade.php
+++ resources/views/quote.blade.php
@@ -2,57 +2,30 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}"">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible"
-          content="ie=edge">
+    <meta charset=" UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ config('app.name') }}</title>
-    @if (app()->isLocal())
-        <script src="https://cdn.tailwindcss.com"></script>
-    @else
-        <link rel="stylesheet"
-              href="{{ asset('css/app.css') }}">
-    @endif
-    <link rel="icon"
-          type="image/png"
-          href="{{ asset('favicon.png') }}" />
+    @vite('resources/css/app.css')
 </head>
 
-<body class="bg-neutral-50 text-neutral-900 antialiased">
-    <nav class="flex place-items-center justify-end space-x-2 p-2">
-        <a href="{{ route('scalar') }}"
-           class="rounded-md px-2 py-2 hover:bg-neutral-100">API Documentation</a>
-        <span>&bullet;</span>
-        <a href="https://github.com/shitware-ltd/hypequotes"
-           class="rounded-md px-2 py-2 hover:bg-neutral-100">GitHub</a>
-    </nav>
-    <main class="grid h-screen place-items-center">
-        <blockquote class="space-y-4 p-12 max-w-4xl">
-            <header>
-                <svg xmlns="http://www.w3.org/2000/svg"
-                     width="38"
-                     height="38"
-                     viewBox="0 0 24 24"
-                     fill="none"
-                     stroke="currentColor"
-                     stroke-width="2"
-                     stroke-linecap="round"
-                     stroke-linejoin="round"
-                     class="text-neutral-300">
-                    <path
-                          d="M3 21c3 0 7-1 7-8V5c0-1.25-.756-2.017-2-2H4c-1.25 0-2 .75-2 1.972V11c0 1.25.75 2 2 2 1 0 1 0 1 1v1c0 1-1 2-2 2s-1 .008-1 1.031V20c0 1 0 1 1 1z" />
-                    <path
-                          d="M15 21c3 0 7-1 7-8V5c0-1.25-.757-2.017-2-2h-4c-1.25 0-2 .75-2 1.972V11c0 1.25.75 2 2 2h.75c0 2.25.25 4-2.75 4v3c0 1 0 1 1 1z" />
-                </svg>
-            </header>
-            <article>
-                <p class="italic sm:text-xl">{{ $quote->quote }}</p>
-            </article>
-            <footer>
-                <p class="text-base font-semibold">- {{ $quote->quotee }}</p>
+<body class="flex flex-col min-h-screen bg-zinc-100 text-zinc-800 font-inter">
+    <main class="flex-grow p-4 place-items-center place-content-center">
+        <blockquote class="flex-col space-y-2 text-xl">
+            <svg  xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 11h-4a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h3a1 1 0 0 1 1 1v6c0 2.667 -1.333 4.333 -4 5" /><path d="M19 11h-4a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h3a1 1 0 0 1 1 1v6c0 2.667 -1.333 4.333 -4 5" /></svg>
+            <p>{{ $quote->quote }}</p>
+            <footer class="text-sm">
+                <cite>- {{ $quote->quotee }}</cite>
             </footer>
         </blockquote>
+    </main>
+
+    <footer class="w-full py-4 px-8 font-semibold bg-zinc-200/40">
+        <nav class="flex justify-end text-sm space-x-4">
+            <a href="{{ route('scalar') }}" class="hover:underline transition-colors">API Reference &rarr;</a>
+            <a href="https://github.com/shitware-ltd/hypequotes" target="_blank" class="hover:underline transition-colors">GitHub &rarr;</a>
+        </nav>
+    </footer>
 </body>
 
 </html>
